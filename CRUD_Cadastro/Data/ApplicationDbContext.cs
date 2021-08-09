347cce802ad6aa111803273674c0eef461d85294
Cicerofer <ciceroferreira2010@hotmail.com>
2021-08-09 20:59:42 -0300

    novo projeto

---
+++ CRUD_Cadastro/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CRUD_Cadastro.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD_Cadastro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ CRUD_Cadastro/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Cadastro.Models
+{
+
+    [Table("Produto")]
+    public class Produto
+    {
+        [Display(Name ="Código")]
+
+        public int Id { get; set; }
+
+
+        [Display(Name ="Descrição")]
+
+        public string Descricao { get; set; }
+    }
+}
